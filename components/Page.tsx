139c9e436453bfe9dd5816e4b7b62578804788d1
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2022-10-31 21:57:52 +0100

    Update body padding

--- components/Page.tsx
+++ components/Page.tsx
@@ -5,6 +5,11 @@ const StyledPage = styled.div`
   padding: 6rem 2rem;
   max-width: 768px;
   margin-right: auto;
+
+  @media screen and (max-width: 1000px) {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
 `;
 
 export default function Page({ children }: { children: React.ReactNode }) {
